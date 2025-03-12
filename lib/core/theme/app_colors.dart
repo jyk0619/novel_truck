3de01dc411034f3907315376a5c3a8b50ce08a56
jyk0619 <jyk990619@naver.com>
2025-03-12 17:53:23 +0900

    홈화면 배치

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  //기본색상
   static const primary = Color(0xFFEE7600);
   static const secondary = Color(0xFF03DAC6);
+  static const surface = Color(0xFFFFFFFF); // 카드 등의 표면 색상
   static const background = Color(0xFFF5F5F5);
+
+  //텍스트 색상
   static const textPrimary = Color(0xFF212121);
   static const textSecondary = Color(0xFF757575);
+  static const textWhite = Color(0xFFFFFFFF); // 밝은 텍스트
+
+  // 기능성 색상
+  static const error = Color(0xFFB00020); // 에러/삭제
+  static const success = Color(0xFF4CAF50); // 성공
+  static const divider = Color(0xFFE0E0E0); // 구분선
 }
--- lib/ui/screens/home/home_view.dart
+++ lib/ui/screens/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Home extends StatelessWidget {
    Home({super.key});
@@ -9,6 +11,123 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(child: Text('Home')),);
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+            child: Column(
+              children: [
+                   Container(
+                     height: 200,
+                     margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                     child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(child: Text('Card 1')),
+                              ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    margin: const EdgeInsets.fromLTRB(5, 0, 0, 5),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.black),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: const Center(child: Text('Card 2')),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                  margin: const EdgeInsets.fromLTRB(5, 5, 0, 0),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.black),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: const Center(child: Text('Card 3')),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                     ),
+                   ),
+                Container(
+                  width: double.infinity,
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Text('최근기록',style: Theme.of(context).textTheme.displayLarge,)),
+        
+                FlutterCarousel(
+                  options: FlutterCarouselOptions(
+                    enableInfiniteScroll: true,
+                    floatingIndicator: false,
+                    showIndicator: false,
+                    height: 150.0,
+                  ),
+                  items: [1,2,3,4,5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+
+                Container(
+                  width: double.infinity,
+                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Text('읽고있는 소설',style: Theme.of(context).textTheme.displayLarge,)),
+
+                FlutterCarousel(
+                  options: FlutterCarouselOptions(
+                    enableInfiniteScroll: true,
+                    floatingIndicator: false,
+                    showIndicator: false,
+                    disableCenter: false,
+                    pageSnapping: true,
+                    height: 150.0,
+                    viewportFraction: 0.3,
+                  ),
+                  items: [1,2,3,4,5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: 100,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+              ],
+        
+            ),
+          ),
+        ),
+      ),);
   }
 }
