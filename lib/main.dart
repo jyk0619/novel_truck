9b5ca39bc4c237679c094a4935c50f8c712310fb
jyk0619 <jyk990619@naver.com>
2025-03-28 16:12:34 +0900

    카카오, 구글 로그인 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:novel_truck/ui/screens/auth/ezsignup_view.dart';
+import 'package:novel_truck/ui/screens/auth/login_viewmodel.dart';
 import 'package:novel_truck/ui/screens/auth/signupform_view.dart';
 import 'package:provider/provider.dart';
 import 'package:novel_truck/ui/screens/auth/signupform_viewmodel.dart';
 import 'package:novel_truck/ui/screens/home/home_view.dart';
 import 'package:novel_truck/ui/screens/auth/login_view.dart';
+import 'package:novel_truck/ui/screens/auth/login_viewmodel.dart';
 import 'package:novel_truck/ui/screens/home/homenav_view.dart';
 import'package:novel_truck/core/theme/app_theme.dart';
 import 'package:novel_truck/ui/screens/auth/signupcom_view.dart';
 import 'package:novel_truck/ui/screens/novel/collection_view.dart';
 import 'package:novel_truck/ui/screens/novel/collectionadd_view.dart';
 import 'package:novel_truck/ui/screens/auth/agree_view.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import  'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(MultiProvider(providers: [ChangeNotifierProvider(create: (_)=>SignUpFormViewModel())
+void main() async{
+  await dotenv.load(fileName:"assets/config/.env");
+  String? kakaoNativeAppKey = dotenv.env['KAKAO_API_KEY'];
+  KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
+  runApp(MultiProvider(providers: [ChangeNotifierProvider(create: (_)=>AuthViewModel())
   ],
     child:MyApp(),
   ));
---
+++ lib/ui/screens/auth/login_viewmodel.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
+enum LoginPlatform { google, kakao, apple, none }
+
+class AuthViewModel extends ChangeNotifier {
+  LoginPlatform _loginPlatform = LoginPlatform.none;
+  LoginPlatform get loginPlatform => _loginPlatform;
+
+  Future<void> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser != null) {
+      print('name = ${googleUser.displayName}');
+      print('email = ${googleUser.email}');
+      print('id = ${googleUser.id}');
+
+      _loginPlatform = LoginPlatform.google;
+      notifyListeners(); // UI 업데이트
+    }
+  }
+
+  Future<void> signInWithKakao() async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+
+      OAuthToken token = isInstalled
+          ? await UserApi.instance.loginWithKakaoTalk()
+          : await UserApi.instance.loginWithKakaoAccount();
+
+      final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
+        },
+      );
+
+      final profileInfo = json.decode(response.body);
+      print('카카오 로그인 성공: $profileInfo');
+
+      _loginPlatform = LoginPlatform.kakao;
+      notifyListeners(); // UI 업데이트
+
+    } catch (error) {
+      print('카카오톡 로그인 실패: $error');
+
+    }
+  }
+
+  Future<void> signOut() async {
+    switch (_loginPlatform) {
+      case LoginPlatform.google:
+        await GoogleSignIn().signOut();
+        break;
+      case LoginPlatform.kakao:
+        await UserApi.instance.logout();
+      case LoginPlatform.apple:
+      case LoginPlatform.none:
+        break;
+    }
+    _loginPlatform = LoginPlatform.none;
+    notifyListeners();
+  }
+}
